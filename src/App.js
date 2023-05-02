86919e65481866e83fdc3715f6037d2a5f2358c1
WIN-OJ111I7NGPD\linmeng <1364876516@qq.com>
2023-05-02 22:01:48 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import 'animate.css'
 
-import { createClient } from "@supabase/supabase-js"
 import * as SupabaseSupabaseJs from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0";
+const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = 'https://eggtdvvjoksncmdbxgsz.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZ3RkdnZqb2tzbmNtZGJ4Z3N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNjE4MDgsImV4cCI6MTk5MzczNzgwOH0.1PNSCvjCrAqMEUPXpPX6qwZCqTRexjWZuhIzSZeorK0'
 const supabase = createClient(supabaseUrl, supabaseKey)
